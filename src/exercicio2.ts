8c0d9484b3b494715db058ce9afd257287407547
victorfldevi <victor.fl.de.vi@gmail.com>
2025-02-24 09:33:15 -0300

    Exercicio 2 concluido

---
+++ src/exercicio2.ts
@@ -0,0 +1,5 @@
+// Exercício 2 - Função Tipada
+function mensagemPersonalizada(nome: string): string {
+    return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
+}
+console.log(mensagemPersonalizada("Ana"));
